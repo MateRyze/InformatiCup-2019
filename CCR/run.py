548c7e0e2b62640b56e1670fcffc3604d2fc1e13
Mateusz Ryzewski <m.ryzewski@web.de>
2018-10-29 23:05:43 +0100

    fix post request

--- CCR/run.py
+++ CCR/run.py
@@ -1,6 +1,9 @@
+
 import requests
-import json
 payload= {'key': 'Engeibei1uok4xaecahChug6eihos0wo', 'image':open('test.png', 'rb')}
-headers = {'Content-Type': 'multipart/form-data'}
-r = requests.post('https://phinau.de/trasi', data=payload, headers=headers)
+#headers = {'Content-Type': 'multipart/form-data'}
+
+r = requests.post('https://phinau.de/trasi', data=payload, files= {'image':open('test.png', 'rb')})
+
 print(r.text, r.json) 
+
