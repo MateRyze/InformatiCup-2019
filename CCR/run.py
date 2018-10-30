9ab350b657758c287942616b691462fe785de889
Mateusz Ryzewski <m.ryzewski@web.de>
2018-10-30 20:15:08 +0100

    map images with classes

--- CCR/run.py
+++ CCR/run.py
@@ -1,9 +1,30 @@
-
 import requests
-payload= {'key': 'Engeibei1uok4xaecahChug6eihos0wo', 'image':open('test.png', 'rb')}
-#headers = {'Content-Type': 'multipart/form-data'}
+import os
+import skimage
+import time
+from PIL import Image
+rootdir = './signs'
+names = set([])
+for subdir, dirs, files in os.walk(rootdir):
+	for file in files:
+		path = os.path.join(subdir, file)
+		print(path)
+		'''
+		# resize images to 64x64
+		data = skimage.io.imread(path)
+		data = skimage.transform.resize(data, (64, 64))
+		skimage.io.imsave(path, data)
+		'''
+		image = Image.open(path)
+		payload= {'key': 'Engeibei1uok4xaecahChug6eihos0wo'}
+		r = requests.post('https://phinau.de/trasi', data=payload, files= {'image': open(path, 'rb')})
+		#print(r.text)
+		for obj in r.json():
+			names.add(obj.get('class'))
+			if(obj.get('confidence') > 0.90):
+				image.save('./recognized/' + obj.get('class') + '.png')
+		time.sleep(1)
+print(names, len(names))
+		
 
-r = requests.post('https://phinau.de/trasi', data=payload, files= {'image':open('test.png', 'rb')})
-
-print(r.text, r.json) 
 
