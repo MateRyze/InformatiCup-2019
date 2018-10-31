358f32b19af6d7655edf20f831420be0f0235052
Mateusz Ryzewski <m.ryzewski@web.de>
2018-10-31 13:08:32 +0100

    add found classes

--- CCR/run.py
+++ CCR/run.py
@@ -18,7 +18,7 @@ for subdir, dirs, files in os.walk(rootdir):
 		image = Image.open(path)
 		payload= {'key': 'Engeibei1uok4xaecahChug6eihos0wo'}
 		r = requests.post('https://phinau.de/trasi', data=payload, files= {'image': open(path, 'rb')})
-		#print(r.text)
+		# get all classes from the API and map images with confidence > 90 %
 		for obj in r.json():
 			names.add(obj.get('class'))
 			if(obj.get('confidence') > 0.90):
