f4183738eb1b53cfb90ddc5802f1cb58f97a1763
Mateusz Ryzewski <m.ryzewski@web.de>
2018-11-01 21:26:46 +0100

    results from testing 1000 photos

--- CCR/run.py
+++ CCR/run.py
@@ -3,28 +3,35 @@ import os
 import skimage
 import time
 from PIL import Image
-rootdir = './signs'
+rootdir = './test'
 names = set([])
+i = 0
 for subdir, dirs, files in os.walk(rootdir):
 	for file in files:
-		path = os.path.join(subdir, file)
-		print(path)
-		'''
-		# resize images to 64x64
-		data = skimage.io.imread(path)
-		data = skimage.transform.resize(data, (64, 64))
-		skimage.io.imsave(path, data)
-		'''
-		image = Image.open(path)
-		payload= {'key': 'Engeibei1uok4xaecahChug6eihos0wo'}
-		r = requests.post('https://phinau.de/trasi', data=payload, files= {'image': open(path, 'rb')})
-		# get all classes from the API and map images with confidence > 90 %
-		for obj in r.json():
-			names.add(obj.get('class'))
-			if(obj.get('confidence') > 0.90):
-				image.save('./recognized/' + obj.get('class') + '.png')
-		time.sleep(1)
+		if (i < 5000):
+			path = os.path.join(subdir, file)
+			print(path)
+			'''
+			# resize images to 64x64
+			data = skimage.io.imread(path)
+			data = skimage.transform.resize(data, (64, 64))
+			skimage.io.imsave(path, data)
+			'''
+			image = Image.open(path)
+			payload= {'key': 'Engeibei1uok4xaecahChug6eihos0wo'}
+			r = requests.post('https://phinau.de/trasi', data=payload, files= {'image': open(path, 'rb')})
+			# get all classes from the API and map images with confidence > 90 %
+			for obj in r.json():
+				names.add(obj.get('class'))
+				if(obj.get('confidence') > 0.90):
+					image.save('./recognized/' + obj.get('class') + '.png')
+			print(i, len(names))
+			time.sleep(1)
+		i = i + 1
+	
 print(names, len(names))
+file = open("classes.txt", "w")
+file.write(names, len(names))
 		
 
 
