e7ab7dc97238515c4bc086da9f92dd872b36ec39
Jan Wolff <janw@mailbox.org>
2018-12-13 08:58:31 +0100

    additional plotting functions

--- evo_algorithm/create_plots_polygon_test.py
+++ evo_algorithm/create_plots_polygon_test.py
@@ -15,4 +15,29 @@ def plotConfidenceAgainstMutation():
                          data=record, legend=False)
     plt.savefig("confidence_vs_mutation.pdf")
 
-plotConfidenceAgainstMutation()
+def plotMutationAgainstClassnames():
+    sns.set()
+    ax = sns.scatterplot(x="mutation", y="classname",
+                         hue="classname", size="confidence", sizes=(10, 100),
+                         data=record, legend=False)
+    plt.savefig("mutation_vs_classnames.pdf")
+
+def plotMutationAgainstEdges():
+    sns.set()
+    ax = sns.scatterplot(x="mutation", y="edges",
+                         hue="classname", size="confidence", sizes=(10, 100),
+                         data=record, legend=False)
+    plt.show()
+
+def plotEdgeCountAgainstClassnames():
+    sns.set()
+    ax = sns.scatterplot(x="edges", y="classname",
+                         hue="classname", size="confidence", sizes=(10, 100),
+                         data=record, legend=False)
+    plt.savefig("edgecount_vs_classnames.pdf")
+
+
+#plotConfidenceAgainstMutation()
+#plotMutationAgainstClassnames()
+plotMutationAgainstEdges()
+#plotEdgeCountAgainstClassnames()
