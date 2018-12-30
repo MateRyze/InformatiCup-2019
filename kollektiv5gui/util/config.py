3256f5ad59c42095a7ec27498eeee4e94c00ceab
Jan Wolff <janw@mailbox.org>
2018-12-30 14:30:49 +0100

    config: allow fallback to default values, allow removing sections

--- kollektiv5gui/util/config.py
+++ kollektiv5gui/util/config.py
@@ -5,18 +5,24 @@ from kollektiv5gui.util.paths import getResourcePath
 
 __DEFAULT_CONFIG_FILENAME = os.path.join(getResourcePath(), 'default.ini')
 __USER_CONFIG_FILENAME = os.path.expanduser('~/.kollektiv5.ini')
+__DEFAULT_CONFIG = None
 __CONFIG = None
 
 def __loadConfig():
     """
     Load an .ini based config file.
     """
-    global __CONFIG
+    global __CONFIG, __DEFAULT_CONFIG
     if not os.path.exists(__USER_CONFIG_FILENAME):
         # if the user has no config, copy the default one to the expected location
         shutil.copy(__DEFAULT_CONFIG_FILENAME, __USER_CONFIG_FILENAME)
     __CONFIG = configparser.ConfigParser()
     __CONFIG.read(__USER_CONFIG_FILENAME)
+
+    # the default config is also always loaded to try and fall back to its values if sections or keys
+    # are missing from the user config
+    __DEFAULT_CONFIG = configparser.ConfigParser()
+    __DEFAULT_CONFIG.read(__DEFAULT_CONFIG_FILENAME)
 
 def get(section, option, type = str):
     """
@@ -25,6 +31,9 @@ def get(section, option, type = str):
     """
     if __CONFIG is None:
         __loadConfig()
+    if not __CONFIG.has_section(section) or not __CONFIG.has_option(section, option):
+        # value is not preset in user config, try and return it from the default one
+        return type(__DEFAULT_CONFIG.get(section, option))
     return type(__CONFIG.get(section, option))
 
 def set(section, option, value):
@@ -33,6 +42,12 @@ def set(section, option, value):
     if not __CONFIG.has_section(section):
         __CONFIG.add_section(section)
     __CONFIG.set(section, option, str(value))
+
+def removeSection(section):
+    if __CONFIG is None:
+        __loadConfig()
+    if __CONFIG.has_section(section):
+        __CONFIG.remove_section(section)
 
 def flush():
     global __CONFIG
