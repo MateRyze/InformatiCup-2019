e284824255741e37d905952618f0a1c0be62f47e
Mateusz Ryzewski <m.ryzewski@web.de>
2018-12-31 19:21:56 +0100

    improve crossover and adjust test functions ... and fix pep8 errors

--- evo_algorithm/ea_combined_aspects/ea_combined.py
+++ evo_algorithm/ea_combined_aspects/ea_combined.py
@@ -17,21 +17,24 @@ api_calls = 0
 stop = False
 MUTATION_RATE = 10
 SHAPE_MUTATION_RATE = 4
-SHAPE_POINTS_COUNT = (5,10)
+SHAPE_POINTS_COUNT = (5, 10)
 SHAPE_COUNT = 2
 
-# defined constraints/aspects for the generation 
-COLORS_RANGE = ((0,150), (0,150), (0, 150))
+# defined constraints/aspects for the generation
+COLORS_RANGE = ((0, 150), (0, 150), (0, 150))
 CONTRAST_RANGE = (100, 300)
 FRAME_SHAPE = [[(16, 16), (48, 16), (48, 48), (16, 48)],
-          [(16, 16), (48, 16), (16, 48), (48, 48)]]
+               [(16, 16), (48, 16), (16, 48), (48, 48)]]
 
 colors = []
 
+
 def randomCoord():
-    return (random.randrange(0, 64),random.randrange(0, 64))
+    return (random.randrange(0, 64), random.randrange(0, 64))
 
 # initial random generation of an image
+
+
 def generateImage():
     # set image format
     img = Image.new('RGB', (64, 64), color='black')
@@ -41,13 +44,14 @@ def generateImage():
     # init shape
     shape = []
     pointCount = random.randrange(SHAPE_POINTS_COUNT[0], SHAPE_POINTS_COUNT[1])
-    for i in range(pointCount): 
-        #idx = random.randrange(0, len(shape))
+    for i in range(pointCount):
         shape.insert(i, (randomCoord()))
-        shape = list(map(lambda x: (mutateCoord(x[0]), mutateCoord(x[1])), shape))
+        shape = list(
+            map(lambda x: (mutateCoord(x[0]), mutateCoord(x[1])), shape))
     drawShapes(draw, colors, shape)
 
     return {"image": img, "confidence": 0, "colors": colors, "class": "", "shape": shape}
+
 
 def drawShapes(draw, colors, shape):
     background = colors[0]
@@ -55,7 +59,9 @@ def drawShapes(draw, colors, shape):
     foreground = colors[1]
     draw.polygon(shape, foreground)
 
-# generate colors with distributed contrast 
+# generate colors with distributed contrast
+
+
 def generateColorsWithContrast(count):
     global colors
     colors = []
@@ -65,7 +71,7 @@ def generateColorsWithContrast(count):
             random.randint(COLORS_RANGE[1][0], COLORS_RANGE[1][1]),
             random.randint(COLORS_RANGE[2][0], COLORS_RANGE[2][1]))
         if(i > 0):
-            # distribute the contrast between the colors 
+            # distribute the contrast between the colors
             while(contrast(color, colors[i-1]) < CONTRAST_RANGE[0]/(count-1) or contrast(color, colors[i-1]) > CONTRAST_RANGE[1]/(count-1)):
                 color = (
                     random.randint(COLORS_RANGE[0][0], COLORS_RANGE[0][1]),
@@ -89,7 +95,7 @@ def evalFitness(population):
             image = individual["image"]
             image.save(name)
             while(True):
-                
+
                 # if(api_calls >= 60):
                 #     time.sleep(60)
                 #     api_calls = 0
@@ -97,7 +103,7 @@ def evalFitness(population):
                 try:
                     payload = {'key': 'Engeibei1uok4xaecahChug6eihos0wo'}
                     r = requests.post('https://phinau.de/trasi',
-                                  data=payload, files={'image': open(name, 'rb')})
+                                      data=payload, files={'image': open(name, 'rb')})
                     api_calls += 1
                     individual["confidence"] = r.json()[0]["confidence"]
                     individual["class"] = str(r.json()[0]["class"])
@@ -111,31 +117,42 @@ def evalFitness(population):
                     print("Retrying...")
                     time.sleep(1)
     return population
-                    
-        
-        
+
+
 # create initial population
 def initPopulation(count):
     print("generating initial population")
+    global population
+    population = []
     for i in range(count):
         population.append(generateImage())
 
+
 # select best individuals from population
-def selection(bestCount):
+def selection(bestCount, sameClassCount):
     print("doing selection")
     global population
     # sort by confidence
-    population.sort(key=lambda individual: individual["confidence"], reverse=True)
-    # take best individuals from same classes  
+    population.sort(
+        key=lambda individual: individual["confidence"],
+        reverse=True
+    )
+    # take best individuals from same classes
     classesContained = []
     selectedPopulation = []
     for individual in population:
-        if(classesContained.count(individual["class"]) < 2):
+        if(classesContained.count(individual["class"]) < sameClassCount):
+            # TODO: fixif(individual["confidence"] < 0.9):
             selectedPopulation.append(individual)
             classesContained.append(individual["class"])
     population = selectedPopulation
     # reduce individuals -> reduce API calls
-    #del population[bestCount*2:]
+    if sameClassCount is 2:
+        # del population[int(INITIAL_POPULATION/2):]
+        print("no individuals deleted from selection")
+    elif sameClassCount is 1:
+        del population[bestCount:]
+
 
 # crossover between individuals in the population
 def crossover():
@@ -148,47 +165,109 @@ def crossover():
         if indi["class"] not in seen:
             duplicates.append([indi])
             seen.append(indi["class"])
-    #print(duplicates)
+    # print(duplicates)
     for index, entry in enumerate(duplicates):
         for individual in population:
             if individual not in entry and individual["class"] == entry[0]["class"]:
                 duplicates[index] = duplicates[index] + [individual]
-    duplicates = [entry for entry in duplicates if len(entry) > 1] 
-    #print(duplicates)
+    duplicates = [entry for entry in duplicates if len(entry) > 1]
+    # print(duplicates)
     beforeCrossover = duplicates
     afterCrossover = []
     # make crossover by concatenating images
-    for entry in duplicates:
+    """     for entry in duplicates:
         images = [entry[0]["image"], entry[1]["image"]]
-        image = Image.new('RGB', (64,64))
+        image = Image.new('RGB', (64, 64))
         image.paste(images[0], (0, 0))
         image.paste(images[1], (32, 0))
-        #image.show()
-        afterCrossover.append({"image": image, "confidence": 0, "colors": None, "class": ""})
-    # crossover by adding polygons
-    """     for entry in duplicates:
+        # image.show()
+        afterCrossover.append({"image": image, "confidence": 0, "colors": None, "class": ""}) """
+
+    # crossover by adding polygons points
+    for entry in duplicates:
         # add polygons
         shape = entry[0]["shape"] + entry[1]["shape"]
         # remove every other point
-        shape = shape[1::2] 
+        # shape = shape[1::2]
         image = Image.new('RGB', (64, 64))
         draw = ImageDraw.Draw(image)
-        drawShapes(draw, entry[0]["colors"], shape)
-        image.show()
+        drawShapes(draw, entry[0]["colors"], entry[0]["shape"])
+        draw.polygon(entry[1]["shape"], entry[1]["colors"][1])
+        # image.show()
         afterCrossover.append(
-            {"image": image, "confidence": 0, "colors": None, "class": ""}) """
-        
+            {
+                "image": image,
+                "confidence": 0,
+                "colors": entry[0]["colors"],
+                "class": "",
+                "shape": shape
+            }
+        )
+        population.append(
+            {
+                "image": image,
+                "confidence": 0,
+                "colors": entry[0]["colors"],
+                "colors2": entry[1]["colors"],
+                "class": "",
+                "shape": entry[0]["shape"],
+                "shape2": entry[1]["shape"]
+            }
+        )
+        # add second crossover child
+        image = Image.new('RGB', (64, 64))
+        draw = ImageDraw.Draw(image)
+        drawShapes(draw, entry[1]["colors"], entry[1]["shape"])
+        draw.polygon(entry[0]["shape"], entry[0]["colors"][1])
+        afterCrossover.append(
+            {
+                "image": image,
+                "confidence": 0,
+                "colors": entry[1]["colors"],
+                "class": "",
+                "shape": shape
+            }
+        )
+        population.append(
+            {
+                "image": image,
+                "confidence": 0,
+                "colors": entry[1]["colors"],
+                "colors2": entry[0]["colors"],
+                "class": "",
+                "shape": entry[1]["shape"],
+                "shape2": entry[0]["shape"]
+            }
+        )
     # crossover by colors average
+    # save results
+    """     directory = "duplicates"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    for i in range(len(afterCrossover)):
+        image = afterCrossover[i]["image"]
+        name = str(directory) + "/" + "img" + \
+            str(i) + "_" + str(afterCrossover[i]["confidence"]
+                                ) + "_" + str(afterCrossover[i]["class"].encode('utf-8')) + ".png"
+        image.save(name) """
     # for testing crossover method
     return {"before": beforeCrossover, "after": afterCrossover}
 
-      
 
 def mutateCoord(oldCoord):
-    return min(63, max(1, oldCoord + random.randint(-SHAPE_MUTATION_RATE, SHAPE_MUTATION_RATE)))
+    return min(
+        63,
+        max(
+            1,
+            oldCoord +
+            random.randint(-SHAPE_MUTATION_RATE, SHAPE_MUTATION_RATE)
+        )
+    )
+
 
 # mutate each individual in the population and delete old population
 def mutate(confidence):
+    print("doing mutation")
     population_size = len(population)
     for i in range(population_size):
         if(population[i]["confidence"] < 0.9):
@@ -196,20 +275,54 @@ def mutate(confidence):
             draw = ImageDraw.Draw(img)
             # mutate colors
             colors = population[i]["colors"]
-            colors = list(map(lambda color: (color[0] + random.randint(-MUTATION_RATE, MUTATION_RATE), color[1] + random.randint(-MUTATION_RATE, MUTATION_RATE), color[2] + random.randint(-MUTATION_RATE, MUTATION_RATE)), colors))
-            #mutate shape
+            colors = list(map(lambda color: (color[0] + random.randint(-MUTATION_RATE, MUTATION_RATE), color[1] + random.randint(
+                -MUTATION_RATE, MUTATION_RATE), color[2] + random.randint(-MUTATION_RATE, MUTATION_RATE)), colors))
+            # mutate shape
             shape = population[i]["shape"]
             if random.random() < 0.5:
                 idx = random.randrange(0, len(shape))
                 if len(shape) > SHAPE_POINTS_COUNT[1] and random.random() < 0.5:
                     del shape[idx]
                 else:
-                    shape.insert(idx,randomCoord())
-            shape = list(map(lambda x: (mutateCoord(x[0]), mutateCoord(x[1])), shape))
-            
+                    shape.insert(idx, randomCoord())
+            shape = list(
+                map(lambda x: (mutateCoord(x[0]), mutateCoord(x[1])), shape))
+
             drawShapes(draw, colors, shape)
-            population.append({"image": img, "confidence": 0,
-                            "colors": colors, "class": "", "shape": shape})
+            if("colors2" in population[i].keys()):
+                # mutate colors
+                colors2 = population[i]["colors2"]
+                colors2 = list(map(lambda color: (color[0] + random.randint(-MUTATION_RATE, MUTATION_RATE), color[1] + random.randint(
+                    -MUTATION_RATE, MUTATION_RATE), color[2] + random.randint(-MUTATION_RATE, MUTATION_RATE)), colors2))
+                # mutate shape
+                shape2 = population[i]["shape2"]
+                if random.random() < 0.5:
+                    idx = random.randrange(0, len(shape2))
+                    if len(shape2) > SHAPE_POINTS_COUNT[1] and random.random() < 0.5:
+                        del shape2[idx]
+                    else:
+                        shape2.insert(idx, randomCoord())
+                shape2 = list(
+                    map(lambda x: (mutateCoord(x[0]), mutateCoord(x[1])), shape2))
+
+                draw.polygon(shape2, colors2[1])
+                population.append({
+                    "image": img,
+                    "confidence": 0,
+                    "colors": colors,
+                    "colors2": colors2,
+                    "class": "",
+                    "shape": shape,
+                    "shape2": shape2
+                })
+            else:
+                population.append({
+                    "image": img,
+                    "confidence": 0,
+                    "colors": colors,
+                    "class": "",
+                    "shape": shape,
+                })
             """ # distribute the contrast between the colors
             while(contrast(colors[0], colors[1]) < CONTRAST_RANGE[0] or contrast(colors[0], colors[1]) > CONTRAST_RANGE[1]):
                     colors = (
@@ -218,16 +331,15 @@ def mutate(confidence):
                         random.randint(COLORS_RANGE[2][0], COLORS_RANGE[2][1])) """
 
             # TODO: add fancy stuff for creativity
-    #del population[:population_size]
+    # del population[:population_size]
 
-    
-
-        
 
 def printResults():
     for individual in population:
-        print("confidence: ", individual["confidence"], " class: ", individual["class"])
+        print("confidence: ",
+              individual["confidence"], " class: ", individual["class"])
     print("..")
+
 
 def getBestResult():
     best = 0
@@ -235,6 +347,8 @@ def getBestResult():
         if(individual["confidence"] > best):
             best = individual["confidence"]
     return best
+
+
 # get the count of images that match the confidence
 def getCountThatMatch(confidence):
     count = 0
@@ -245,35 +359,43 @@ def getCountThatMatch(confidence):
 
 
 # init parameters
-INITIAL_POPULATION = 60 # EXPERIMENT
+INITIAL_POPULATION = 30  # EXPERIMENT
 SELECTED_COUNT = 5  # specification
-DESIRED_CONFIDENCE = 0.9 # specification
+DESIRED_CONFIDENCE = 0.9  # specification
+
 
 def addRandomImage():
     population.append(generateImage())
 
 # run evolutionary algorithm (init -> selection -> loop(crossover-> mutate -> selection) until confidence matches all images)
+
+
 def runEvoAlgorithm():
     global population
     initPopulation(INITIAL_POPULATION)
     evalFitness(population)
-    selection(SELECTED_COUNT)
+    selection(SELECTED_COUNT, 2)
+    printResults()
+    crossover()
+    evalFitness(population)
+    selection(SELECTED_COUNT, 1)
     printResults()
     matchCount = getCountThatMatch(DESIRED_CONFIDENCE)
-    while matchCount < SELECTED_COUNT and stop == False:
-        # crossover()
+    while matchCount < SELECTED_COUNT and stop is False:
         mutate(DESIRED_CONFIDENCE)
         evalFitness(population)
-        selection(SELECTED_COUNT)
-        if (stop == False):
+        selection(SELECTED_COUNT, 1)
+        if stop is False:
             printResults()
-        # avoid local maxima by adding new random image    
+        # avoid local maxima by adding new random image
         newMatchCount = getCountThatMatch(DESIRED_CONFIDENCE)
         if newMatchCount == matchCount:
             addRandomImage()
         matchCount = newMatchCount
 
 # save generated images with desired confidence
+
+
 def saveImages():
     directory = "generated_api_calls_" + str(api_calls)
     if not os.path.exists(directory):
@@ -282,9 +404,10 @@ def saveImages():
         image = population[i]["image"]
         name = str(directory) + "/" + "img" + \
             str(i) + "_" + str(population[i]["confidence"]
-                                ) + "_" + str(population[i]["class"].encode('utf-8')) + ".png"
+                               ) + "_" + str(population[i]["class"]) + ".png"
         image.save(name)
-        webbrowser.open(name)
+        # webbrowser.open(name)
+
 
 def evalInitialPopulation():
     global population
@@ -294,8 +417,9 @@ def evalInitialPopulation():
     printResults()
     saveImages()
 
+
 if __name__ == '__main__':
     runEvoAlgorithm()
     saveImages()
-    #evalInitialPopulation()
+    # evalInitialPopulation()
     print("api calls: ", api_calls)
--- evo_algorithm/ea_combined_aspects/test_ea_combined.py
+++ evo_algorithm/ea_combined_aspects/test_ea_combined.py
@@ -26,21 +26,40 @@ class Test_EA(unittest.TestCase):
                          ea_combined.SELECTED_COUNT)
 
     def test_crossover(self):
-        ea_combined.initPopulation(120)
-        ea_combined.evalFitness(ea_combined.population)
-        ea_combined.selection(ea_combined.SELECTED_COUNT)
-        crossoverResults = ea_combined.crossover()
-        # crossover should improve confidence
-        results = ea_combined.evalFitness(crossoverResults["after"])
-        print("before: ", crossoverResults["before"])
-        print("after: ", results)
         resultsPassed = 0
-        for index, entry in enumerate(results):
-            confidence = entry["confidence"]
-            if confidence > crossoverResults["before"][index][0]["confidence"] and confidence > crossoverResults["before"][index][1]["confidence"] and entry["class"] == crossoverResults["before"][index][0]["class"]:
-                resultsPassed += 1
-        self.assertEqual(resultsPassed, len(crossoverResults["before"]), "no improvement after crossover")
+        crossoverCount = 0
+        # loop for statistical results
+        for i in range(10):
+            ea_combined.initPopulation(50)
+            ea_combined.evalFitness(ea_combined.population)
+            ea_combined.selection(ea_combined.SELECTED_COUNT, 2)
+            crossoverResults = ea_combined.crossover()
+            self.assertGreater(len(crossoverResults["before"]), 0,
+                               "Crossover not possible, no images with same classes exist!")
+            self.assertGreater(len(crossoverResults["after"]), 1,
+                               "No crossover results created!")
+            # crossover should improve confidence
+            results = ea_combined.evalFitness(crossoverResults["after"])
+            print("before: ", len(
+                crossoverResults["before"]),  crossoverResults["before"])
+            print("after: ", len(results), results)
 
+            crossoverCount += len(crossoverResults["before"])
+            # [1,2,3,4] -> [(1, 2), (3, 4)]
+            results = zip(results[0::2], results[1::2])
+            for index, children in enumerate(results):
+                parents = crossoverResults["before"][index]
+                if (
+                    children[0]["confidence"] > parents[0]["confidence"] and
+                    children[0]["confidence"] > parents[1]["confidence"] and
+                    children[0]["class"] == parents[0]["class"] or
+                    children[1]["confidence"] > parents[0]["confidence"] and
+                    children[1]["confidence"] > parents[1]["confidence"] and
+                    children[1]["class"] == parents[0]["class"]
+                ):
+                    resultsPassed += 1
+        self.assertEqual(resultsPassed, crossoverCount, "Improvement: " +
+                         str(resultsPassed/crossoverCount*100) + "%")
 
     def test_mutate(self):
         ea_combined.initPopulation(ea_combined.INITIAL_POPULATION)
@@ -49,15 +68,19 @@ class Test_EA(unittest.TestCase):
         ea_combined.mutate(ea_combined.DESIRED_CONFIDENCE)
         self.assertEqual(len(ea_combined.population),
                          ea_combined.INITIAL_POPULATION)
-    
+
     def test_run_ea(self):
         ea_combined.runEvoAlgorithm()
         # check if confidence is at least 90 % for all images (specification)
-        self.assertTrue(all(individual["confidence"] >= 0.9 for individual in ea_combined.population), "The confidence is not at least 90 percent for 5 images!")
+        self.assertTrue(all(individual["confidence"] >= 0.9 for individual in ea_combined.population),
+                        "The confidence is not at least 90 percent for 5 images!")
         # check for different classes
-        self.assertTrue(len(set(individual["class"] for individual in ea_combined.population)) == 5, "Generated images contain class duplicates!")
+        self.assertTrue(len(set(individual["class"] for individual in ea_combined.population))
+                        == 5, "Generated images contain class duplicates!")
         # check for api call limit (FAST SOLUTION -> quality aspect)
-        self.assertGreater(61, ea_combined.api_calls, "To much API calls -> slow solution :(")
+        self.assertGreater(61, ea_combined.api_calls,
+                           "To much API calls -> slow solution :(")
+
 
 if __name__ == '__main__':
     unittest.main()
