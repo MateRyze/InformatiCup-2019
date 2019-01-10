c06c14d43ad0984afdffd506b861afeb4efe1344
Moheb <home@home.de>
2019-01-10 19:10:28 +0100

    Finale Phase

--- evo_algorithm/ea_combined_aspects/ea_combined.py
+++ evo_algorithm/ea_combined_aspects/ea_combined.py
@@ -322,15 +322,6 @@ def mutate(confidence):
 
             population.append({"image": img, "confidence": 0,
                                "colors": colors, "class": "", "shape": shape, "texts": texts})
-
-            """ # distribute the contrast between the colors
-            while(contrast(colors[0], colors[1]) < CONTRAST_RANGE[0] or contrast(colors[0], colors[1]) > CONTRAST_RANGE[1]):
-                    colors = (
-                        random.randint(COLORS_RANGE[0][0], COLORS_RANGE[0][1]),
-                        random.randint(COLORS_RANGE[1][0], COLORS_RANGE[1][1]),
-                        random.randint(COLORS_RANGE[2][0], COLORS_RANGE[2][1])) """
-
-
         
 
 def printResults():
